90320f0bdcac06355f56f0b3d80e33f760eecb73
jeeyeon <jeeyeon118@gmail.com>
2023-11-01 21:53:16 +0900

    20231101-h2

--- news_step6/index.js
+++ news_step6/index.js
@@ -1,59 +1,55 @@
-// 라우터를 통한 함수호출식으로 변경
-//getData-API 가져오기
-//getNewsData-타이틀, 코멘츠 by id
-// newsDetail-해당아이디로 교체하여 내용을 h1 으로 가져오기<)
-// 해시변경시 뉴스디테일 실행, 겟뉴스데이터 실행
-
-const ajax = new XMLHttpRequest()
-const CONTENT_URL = 'https://api.hnpwa.com/v0/news/1.json'
+const ajax = new XMLHttpRequest() //브라우저가 비동기처리를 위해 제공하는 내장객체임
 const TITLE_URL = 'https://api.hnpwa.com/v0/news/1.json'
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'
 const container = document.getElementById('root')
-const content = document.createElement('div')
-const ul = document.createElement('ul')
+const ul = document.createElement('ul') //DOM  API가지고 태그를 만드니까 직관적이지 않다. DOM Tree그려지지 않아서.
 
-//중복되는 부분 getData 함수로 만들기
 getData = (url) => {
   ajax.open('GET', url, false)
-  ajax.send()
+  ajax.send() //서버측에 요청 보내고 응답 기다리는 중
   return JSON.parse(ajax.response)
 }
 
-//getNewsData-타이틀, 코멘츠 by id
 getNewsData = () => {
-  const newsList = getData(TITLE_URL)
+  const newsData = getData(TITLE_URL)
   const newsPage = []
   newsPage.push('<ul>')
-  for (let i = 0; i < newsList.length; i++) {
+  for (let i = 0; i < 10; i++) {
     newsPage.push(`
       <li>
-        <a href="#${newsList[i].id}">
-        ${newsList[i].title},(😍${newsList[i].comments_count})
+        <a href="#${newsData[i].id}">
+          ${newsData[i].title}, (😍${newsData[i].comments_count})
         </a>
-        </li>
+      </li>
     `)
-  }
+  } //end of for
   newsPage.push('</ul>')
   container.innerHTML = newsPage.join('')
 }
 
-//newsDetail-해당아이디로 교체하여 내용을 h1 으로 가져오기)
 newsDetail = () => {
   const id = location.hash.substring(1)
+  console.log(id)
   const ncontent = getData(CONTENT_URL.replace('@id', id))
   container.innerHTML = `
-    <h1>${ncontent.title}></h1>
+    <h1>${ncontent.title}</h1>
     <div><a href="#">목록으로</a></div>
   `
 }
 
 router = () => {
   const routerPath = location.hash
+  // 타입까지 같니? ===
+  //#만 있으면 빈값을 반환함 -> 글목록보기로 이동
   if (routerPath === '') {
-    getNewsData()
+    getNewsData() //글 목록보기
   } else {
-    newsDetail()
+    newsDetail() //글 내용보기
   }
 }
 
+//함수 호출 가능
 window.addEventListener('hashchange', router)
 router()
+// window.addEventListener('hashchange', newsDetail);
+// getNewsData();

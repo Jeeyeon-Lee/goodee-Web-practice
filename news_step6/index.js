a462a997164d4e48fc605b863db1f04f707fa6ca
Jeeyeon-Lee <jeeyeon118@gmail.com>
2023-11-01 19:47:20 +0900

    20231101-2

--- news_step6/index.js
+++ news_step6/index.js
@@ -1,8 +1,15 @@
+// 라우터를 통한 함수호출식으로 변경
+//getData-API 가져오기
+//getNewsData-타이틀, 코멘츠 by id
+// newsDetail-해당아이디로 교체하여 내용을 h1 으로 가져오기<)
+// 해시변경시 뉴스디테일 실행, 겟뉴스데이터 실행
+
 const ajax = new XMLHttpRequest()
 const CONTENT_URL = 'https://api.hnpwa.com/v0/news/1.json'
 const TITLE_URL = 'https://api.hnpwa.com/v0/news/1.json'
 const container = document.getElementById('root')
 const content = document.createElement('div')
+const ul = document.createElement('ul')
 
 //중복되는 부분 getData 함수로 만들기
 getData = (url) => {
@@ -10,32 +17,43 @@ getData = (url) => {
   ajax.send()
   return JSON.parse(ajax.response)
 }
-//함수로 변경
-const newsList = getData(TITLE_URL)
-console.log(newsList)
-const ul = document.createElement('ul')
 
-window.addEventListener('hashchange', () => {
-  //hash값 가져올 건데, 첫자리빼고 가져오는거야(# 빼고)
+//getNewsData-타이틀, 코멘츠 by id
+getNewsData = () => {
+  const newsList = getData(TITLE_URL)
+  const newsPage = []
+  newsPage.push('<ul>')
+  for (let i = 0; i < newsList.length; i++) {
+    newsPage.push(`
+      <li>
+        <a href="#${newsList[i].id}">
+        ${newsList[i].title},(😍${newsList[i].comments_count})
+        </a>
+        </li>
+    `)
+  }
+  newsPage.push('</ul>')
+  container.innerHTML = newsPage.join('')
+}
+
+//newsDetail-해당아이디로 교체하여 내용을 h1 으로 가져오기)
+newsDetail = () => {
   const id = location.hash.substring(1)
   const ncontent = getData(CONTENT_URL.replace('@id', id))
-  const title = document.createElement('h1')
-  title.innerHTML = ncontent.title
-  content.appendChild(title)
-})
-
-// for문 배열로 만들어서 처리하기 push, join 사용
-// 배열로 담을 상수 선언 & 한번만 실행되어야하는건('<ul>' '</ul>') for문 밖에 작성 필요
-const newsPage = []
-newsPage.push('<ul>')
-for (let i = 0; i < newsList.length; i++) {
-  newsPage.push(`
-    <li>
-      <a href="#${newsList[i].id}">
-      ${newsList[i].title},(😍${newsList[i].comments_count})
-      </a>
-      </li>
-  `)
+  container.innerHTML = `
+    <h1>${ncontent.title}></h1>
+    <div><a href="#">목록으로</a></div>
+  `
 }
-newsPage.push('</ul>')
-container.innerHTML = newsPage.join('')
+
+router = () => {
+  const routerPath = location.hash
+  if (routerPath === '') {
+    getNewsData()
+  } else {
+    newsDetail()
+  }
+}
+
+window.addEventListener('hashchange', router)
+router()

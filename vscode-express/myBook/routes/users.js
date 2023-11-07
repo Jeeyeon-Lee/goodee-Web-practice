fe1e3536c0cd928a9b4ea188ab606ad4fe57946d
jeeyeon <jeeyeon118@gmail.com>
2023-11-07 23:05:58 +0900

    20231107 h2

--- vscode-express/myBook/routes/users.js
+++ vscode-express/myBook/routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource')
+})
+router.get('/cart', function (req, res, next) {
+  // render(위치, 타이틀, 페이지네임)
+  res.render('index', { title: '장바구니', pageName: 'users/cart.ejs' })
+})
 
-module.exports = router;
+module.exports = router
